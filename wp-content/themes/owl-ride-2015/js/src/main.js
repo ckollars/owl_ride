ea7833cd4c31d5e06e936e65f6367d8c5a02aaa1
Christopher Kollars <chris@christopherkollars.com>
2015-06-15 08:51:26 -0500

    stuffs

---
+++ wp-content/themes/owl-ride-2015/js/src/main.js
@@ -0,0 +1,19 @@
+
+/**
+ * DOCUMENT READY
+ * -------------------------------------------------------------------
+ *
+ */
+$(document).ready(function () {
+
+  console.log('load');
+  var mobile = $('.btn--mobile');
+
+  mobile.on("click", function() {
+    var $self = $(this);
+
+    $self.toggleClass('active');
+    $self.next().toggleClass('menu--show');
+  });
+
+});
